bb914924f4c48ac0438607b8ee94f5fae9d2fc29
ryananthonydiaz <email@ryananthonydiaz.com>
2020-03-04 18:41:47 -0800

    cors added to server and router removed

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
+
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
-const router = require('./router');
-
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
 
+app.use(cors());
+
+const io = socketio(server);
 io.on('connect', (socket) => {
 
   socket.on('join', ({ name, room }, callback) => {
@@ -47,7 +49,5 @@ io.on('connect', (socket) => {
     }
   });
 });
-
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on Port: ${PORT}`));
